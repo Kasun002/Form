704e7d68d7d2003a23caa4f3cd1b9b89209241cd
kasun002 <abaywardanakasun@gmail.com>
2017-04-23 19:59:16 +0530

    Login registration togel added

--- src/app/authintication/authintication.component.ts
+++ src/app/authintication/authintication.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthinticationComponent implements OnInit {
 
+  formTogel:boolean=true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  formSelector(type){
+    if(type==='log'){
+      this.formTogel=true;
+    }else if(type==='reg'){
+      this.formTogel=false;
+    }else{
+      this.formTogel=true;
+    }
   }
 
 }
