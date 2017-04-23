36b875e0c6bb273ac26bc3345f4c707ea376c512
kasun002 <abaywardanakasun@gmail.com>
2017-04-23 14:16:39 +0530

    Bootswrap theme added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule } from 'ngx-bootstrap';
 import { appRouting, appRoutingProviders }  from './app.routes';
 import { AppComponent } from './app.component';
 
@@ -16,7 +17,8 @@ import { AuthinticationModule } from "./authintication/authintication.module";
     FormsModule,
     HttpModule,
     appRouting,
-    AuthinticationModule
+    AuthinticationModule,
+    AlertModule.forRoot(),
   ],
   providers: [appRoutingProviders],
   bootstrap: [AppComponent]
