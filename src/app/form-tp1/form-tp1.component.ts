f9eb385148f6cee063599cb64a1bf5542f1c2706
kasun@upay.lk <kasun@upay.lk>
2017-05-04 00:47:05 +0530

    Validation

--- src/app/form-tp1/form-tp1.component.ts
+++ src/app/form-tp1/form-tp1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-tp1',
@@ -6,10 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-tp1.component.css']
 })
 export class FormTp1Component implements OnInit {
+  private formStatus:number=1;
+  public form1: FormGroup;
+  public form2: FormGroup;
+  public form3: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb1: FormBuilder,
+    private fb2: FormBuilder,
+    private fb3: FormBuilder,
+    private router: Router) { 
+      this.formValidation();
+      console.log(this.form1.get('name').invalid);
+    }
 
   ngOnInit() {
+    //this.form1Validation();
+  }
+
+  formNext(val){
+    console.log(this.form1.get('name').invalid);
+    this.formStatus=val;
+  }
+
+  formValidation(){
+    this.form1 = this.fb1.group({
+      name: ['', [Validators.required]],
+      nic: ['', [Validators.required, Validators.pattern(/([0-9]{12})|([0-9]{9}[vVxX])/)]],
+      mobile: ['', [Validators.required, Validators.pattern(/^[7]\d{8}$/), Validators.maxLength(9)]],
+      email: ['', [Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)]],
+      youAre: ['', [Validators.required]]
+    });
+
+    /*
+    *TO DO
+    *Add the form controlers according to the above and fill the html
+    */
+    this.form2 = this.fb2.group({
+
+    });
+
+    this.form3 = this.fb3.group({
+
+    });
+
   }
 
 }
--- src/app/form-tp1/form-tp1.module.ts
+++ src/app/form-tp1/form-tp1.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormTp1Component } from './form-tp1.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [FormTp1Component]
 })
