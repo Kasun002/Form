11e53552d95cdfa4d11695a5081c5fa0af87ce0a
kasun@upay.lk <kasun@upay.lk>
2017-05-25 22:38:21 +0530

    Date time pickers and multipal select add

--- src/app/form-tp1/form-tp1.component.ts
+++ src/app/form-tp1/form-tp1.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { IMultiSelectOption, IMultiSelectSettings } from 'angular-2-dropdown-multiselect';
 import {ValidationService} from './validation.service';
+import { DatePickerOptions, DateModel } from 'ng2-datepicker';
 
 @Component({
   selector: 'app-form-tp1',
@@ -10,10 +11,30 @@ import {ValidationService} from './validation.service';
   styleUrls: ['./form-tp1.component.css']
 })
 export class FormTp1Component implements OnInit {
-  private formStatus:number=1;
+  private formStatus: number = 1;
   public form1: FormGroup;
   public form2: FormGroup;
   public form3: FormGroup;
+  optionsModel: number[];
+
+  date: DateModel;
+  options: DatePickerOptions;
+  momentValue;
+
+  myOptions: IMultiSelectOption[] = [
+    { id: 1, name: 'Car brands', isLabel: true },
+    { id: 2, name: 'Volvo', parentId: 1 },
+    { id: 3, name: 'Honda', parentId: 1 },
+    { id: 4, name: 'BMW', parentId: 1 },
+    { id: 5, name: 'Colors', isLabel: true },
+    { id: 6, name: 'Blue', parentId: 5 },
+    { id: 7, name: 'Red', parentId: 5 },
+    { id: 8, name: 'White', parentId: 5 }
+];
+
+mySettings: IMultiSelectSettings = {
+    enableSearch: true,
+};
 
   constructor(
     private fb1: FormBuilder,
@@ -23,6 +44,7 @@ export class FormTp1Component implements OnInit {
     private router: Router) { 
       this.formValidation();
       console.log(this.form1.get('name').invalid);
+      this.options = new DatePickerOptions();
     }
 
   ngOnInit() {
@@ -33,6 +55,15 @@ export class FormTp1Component implements OnInit {
     console.log(this.form1.get('name').invalid);
     this.formStatus=val;
   }
+
+  onChange($event){
+
+  }
+
+  public setMoment(moment: any): any {
+    this.momentValue = moment;
+    // Do whatever you want to the return object 'moment'
+}
 
   formValidation(){
     this.form1 = this.fb1.group({
